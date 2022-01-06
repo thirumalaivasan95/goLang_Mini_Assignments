56955118ce19faf7d7e4c15534daa6dc3ed9f895
Thirumalai Vasan <thirumalaithiruvasan@gmail.com>
2022-01-06 14:56:00 +0530

    3rd assignment correction

--- 3.go
+++ 3.go
@@ -1,43 +1,38 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func switchCase(s string) {
-	fmt.Printf("%s\n\n", s)
+type RuneTypeCounts struct {
+	vowels, consonants, digits, specialCharacter int
+}
 
-	var vowel, consonant, digits, specialCharacter int
-
-	for i := 0; i < len(s); i++ {
-		switch s[i] {
-		case 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y':
-			vowel += 1
-
-		case 'b', 'B', 'c', 'C', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H',
-			'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'p', 'P',
-			'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'v', 'V', 'w', 'W',
-			'x', 'X', 'z', 'Z':
-			consonant += 1
-
-		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
-			digits += 1
-
-		default:
-			specialCharacter += 1
+func analyseText(text string) RuneTypeCounts {
+	var result RuneTypeCounts
+	m := make(map[rune]*int)
+	for _, r := range "aeiou" {
+		m[r] = &result.vowels
+	}
+	for _, r := range "bcdfghjklmnpqrstvwxyz" {
+		m[r] = &result.consonants
+	}
+	for _, r := range "0123456789" {
+		m[r] = &result.digits
+	}
+	for _, r := range text {
+		ref, inMap := m[r]
+		if inMap {
+			*ref++
+		} else {
+			result.specialCharacter++
 		}
 	}
-	fmt.Printf("Vowels: %d\n", vowel)
-	fmt.Printf("Consonant: %d\n", consonant)
-	fmt.Printf("Digits: %d\n", digits)
-	fmt.Printf("Special Characters: %d\n", specialCharacter)
+	return result
 }
 
 func main() {
 	fmt.Print("\033[H\033[2J")
-
-	var str string = "Quest-global company1111"
-	switchCase(str)
-
+	rt := analyseText("Quest-global company1111")
+	fmt.Printf("%+v", rt)
+	fmt.Println()
 	fmt.Println()
 }
